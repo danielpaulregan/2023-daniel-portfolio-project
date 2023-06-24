355604e031547349186848cd4237aa6dc64fedd3
Daniel Regan <51374256+danielpaulregan@users.noreply.github.com>
2023-06-23 20:34:34 -0700

    Rewrite for responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,41 +18,49 @@ export default function Home() {
         <Image src={bg} alt={''} quality={100} />
       </div>
 
-      <nav className="bg-black bg-opacity-60 pt-10">
-        <div id="ghIcon">
-          <div id="circularIcon" className="px-10 float-left">
-            <a href="https://github.com/danielpaulregan">
-              <Image src={ghIconStatic} alt={''} quality={100} />
-              <p className="py-2">Check out my Github!</p>
-            </a>
-          </div>
+      <div className="bg-black bg-opacity-60">
+        <div id="nameAndTitle">
+          <h1 className="font-semibold text-center w-full">Daniel Regan</h1>
+          <h2 className="text-center w-full">Full-Stack Software Engineer & IT Professional</h2>
         </div>
-        <div id="liIcon">
-          <div id="circularIcon" className="px-10 float-right">
-            <a href="https://www.linkedin.com/in/danielpaulregan/">
-              <Image src={liIconStatic} alt={''} quality={100} />
-              <p className="py-2">Check out my LinkedIn!</p>
-            </a>
-          </div>
-        </div>
-
-        <h1 className="text-7xl font-semibold text-center">Daniel Regan</h1>
-        <h2 className="pb-10 text-2xl pt-5 text-center">Full-Stack Software Engineer & IT Professional</h2>
-
-        <ul id="navigationBar" className="text-center bg-opacity-60 outline outline-3">
-          <li><a href="#top">Home</a></li>
-          <li><a href="">About</a></li>
-          <li><a href="">Projects</a></li>
-          <li><a href="">Get in Touch</a></li>
-        </ul>    
-      </nav>
-
-      <div>
-        <h2 className="pt-20 text-center text-2xl drop-shadow-lg">Who is Daniel?</h2>
-        <p className="text-center text-lg drop-shadow-lg pt-5">Daniel is a guy who likes to learn and needs a job.</p>
+        <nav>
+          <ul id="navigationBar" className="text-center outline outline-3">
+            <li><a href="#top">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Projects</a></li>
+            <li><a href="">Get in Touch</a></li>
+          </ul>    
+        </nav>
       </div>
 
-      <a target="_blank" href="https://icons8.com">Icons supplied by Icons8</a>
+      <div>
+        <h2 className="text-center">Who is Daniel?</h2>
+        <p className="text-center">Daniel is a guy who likes to learn and needs a job.</p>
+      </div>
+
+      <div>
+        <div id="ghReference">
+          <div id="circularIcon" className="text-center">
+            <a href="https://github.com/danielpaulregan">
+              <Image src={ghIconStatic} alt={''} quality={100} className="float-left" />
+              <span>My Github Profile</span>
+            </a>
+          </div>
+        </div>
+
+        <div id="liReference">
+          <div id="circularIcon" className="">
+            <a href="https://www.linkedin.com/in/danielpaulregan/">
+              <Image src={liIconStatic} alt={''} quality={100} className="float-left" />
+              <span>Connect (or hire me) on LinkedIn!</span>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <footer className="align-bottom">
+        <h5><a target="_blank" href="https://icons8.com">Icons supplied by Icons8</a></h5>
+      </footer>
     </main>
   )
 }
